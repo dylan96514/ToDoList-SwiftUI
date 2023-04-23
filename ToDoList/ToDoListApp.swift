2e747d282cb51e22cf83b1b3086fb88179b94b6f
Dylan <dylan96514@gmail.com>
2023-04-23 14:27:46 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Dylan Kim on 2023/04/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
