ec8cc04f0ac15f74cf2211629a15463aa6c078f2
Dylan <dylan96514@gmail.com>
2023-04-23 16:51:51 -0400

    Week 6

--- ToDoList/DetailedView.swift
+++ ToDoList/DetailedView.swift
@@ -9,30 +9,58 @@ import SwiftUI
 
 struct DetailedView: View {
     @Environment(\.dismiss) private var dismiss
+    @State private var toDo = ""
+    @State private var reminderIsOn = false
+    @State private var dueDate = Date.now + (60*60*24)
+    @State private var notes = ""
+    @State private var isCompleted = false
     
     var passedValue: String
     
     var body: some View {
-        VStack{
-            Image(systemName: "swift")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.orange)
-            
-            Text("You Are a Swifty Legend!\n And you passed over the value \(passedValue )")
-                .font(.largeTitle)
-                .multilineTextAlignment(.center)
-            
-            Spacer()
-            
-            Button("Get Back"){
-                dismiss() 
+        NavigationStack{
+            List{
+                TextField("Enter To Do Here", text: $toDo)
+                    .font(.title)
+                    .textFieldStyle(.roundedBorder)
+                    .padding(.vertical)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Set Reminder", isOn: $reminderIsOn)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+                
+                DatePicker("Date", selection: $dueDate)
+                    .listRowSeparator(.hidden)
+                    .disabled(!reminderIsOn)
+                
+                Text("Notes: ")
+                    .padding(.top)
+                TextField("Notes", text: $notes, axis: .vertical)
+                    .textFieldStyle(.roundedBorder)
+                    .listRowSeparator(.hidden)
+                
+                Toggle("Completed", isOn: $isCompleted)
+                    .padding(.top)
+                    .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+        }
+        .navigationBarBackButtonHidden()
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button("Cancel") {
+                    dismiss()
+                }
             }
             
-            
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button("Save") {
+                    //TODO: Save Code
+                }
+            }
         }
-        .padding()
-     }
+    }
 }
 
 struct DetailedView_Previews: PreviewProvider {
